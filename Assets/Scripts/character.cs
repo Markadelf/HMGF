ac7466e985e1422564efe48507bd1bef780841a7
Mark Obeldobel <markobeldobel@yahoo.com>
2018-02-08 00:27:40 -0500

    Set up folders and created a test scene. Test scene is not yet finished.

---
+++ Assets/Scripts/character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Vertical") > 0)
+		{
+			transform.position += transform.forward * Time.deltaTime;
+		}
+		if (Input.GetAxis("Vertical") < 0)
+		{
+			transform.position -= transform.forward * Time.deltaTime;
+		}
+		if (Input.GetAxis("Horizontal") > 0) 
+		{
+			transform.Rotate(transform.up * 40.0f * Time.deltaTime);
+		}
+		if (Input.GetAxis("Horizontal") < 0) 
+		{
+			transform.Rotate(-transform.up * 40.0f * Time.deltaTime);
+		}
+
+		Debug.Log("Forward: " + transform.forward.ToString("F4"));
+		Debug.Log("Up: " + transform.up.ToString("F4"));
+	}
+}
