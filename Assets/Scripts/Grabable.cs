26eba5cf6b104336296f5ca989c773ac2b6945e0
Jaklans <jxs2828@g.rit.edu>
2018-02-16 16:17:44 -0500

    Created Grab and Grabable

---
+++ Assets/Scripts/Grabable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabable : MonoBehaviour
+{
+    Transform normalParent;
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    public void Grab(GameObject grabber)
+    {
+        normalParent = transform.parent;
+
+        transform.parent = grabber.transform;
+    }
+
+    public void Release()
+    {
+        transform.parent = normalParent;
+    }
+}
---
+++ Assets/Scripts/Grabber.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour
+{
+    bool isGrabObject;
+    Grabable grabbedObject;
+
+	void Start ()
+    {
+        isGrabObject = false;
+	}
+	
+	
+	void Update ()
+    {
+		if(isGrabObject && (!Input.GetButton("14") && !Input.GetButton("15")))
+        {
+            grabbedObject.Release();
+            isGrabObject = false;
+        }
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (isGrabObject) return;
+
+        if(Input.GetButton("14") || Input.GetButton("15"))
+        {
+            grabbedObject = collision.gameObject.GetComponent<Grabable>();
+
+            if (grabbedObject = null) return;
+
+            grabbedObject.Grab(gameObject);
+
+            isGrabObject = true;
+        }
+    }
+}
