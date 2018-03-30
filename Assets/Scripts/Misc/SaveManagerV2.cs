e5bb63d6ea48ff615830b2bc9147d323feb2d514
Conner Catanese (RIT Student) <clc8254@ad.rit.edu>
2018-03-30 15:49:54 -0400

    save manager v2 rewrite

--- Assets/Scripts/Misc/SaveManagerV2.cs
+++ Assets/Scripts/Misc/SaveManagerV2.cs
@@ -13,43 +13,116 @@ public class SaveManagerV2 : MonoBehaviour
     // Save
     public void Save()
     {
-        // get all saveable objects
-        // run their save scripts and hold that data
-        // make an instance of ObjectSaveData and put that data in their, paired with the name
-        // serialize the ObjectSaveData to file
+        SaveFileData sd = new SaveFileData();
+
+        // get all objects tagged "door"
+        GameObject[] doors = GameObject.FindGameObjectsWithTag("door");
+        foreach (GameObject door in doors)
+        {
+            sd.doors.Add(door.name, JsonUtility.ToJson(door));
+        }
+
+        // get all objects tagged "keyItem"
+        GameObject[] keyItems = GameObject.FindGameObjectsWithTag("keyItem");
+        foreach (GameObject keyItem in keyItems)
+        {
+            sd.keyItems.Add(keyItem.name, JsonUtility.ToJson(keyItem));
+        }
+
+        // get object tagged "player"
+        GameObject player = GameObject.FindGameObjectWithTag("player");
+        sd.player.Add(player.name, JsonUtility.ToJson(player));
+
+        // get all objects tagged "moveableObject"
+        GameObject[] moveableObjects = GameObject.FindGameObjectsWithTag("moveableObject");
+        foreach (GameObject moveableObject in moveableObjects)
+        {
+            sd.moveableObjects.Add(moveableObject.name, JsonUtility.ToJson(moveableObject));
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = File.Open(saveFilePath, FileMode.OpenOrCreate);
+        bf.Serialize(fs, sd);//write to file
+        fs.Close();
     }
 
     // Load
     public void Load()
     {
-        // deserialize the save file into an ObjectSaveData instance
-        // iterate through the trackedObjects
-        // call Load() on each object, passing its save data (matching index in objectSaveData)
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = File.Open(saveFilePath, FileMode.Open);
+        SaveFileData sd = (SaveFileData)bf.Deserialize(fs);//read from file
+        fs.Close();
+
+        // load doors
+        foreach (var doorName in sd.doors.Keys)
+        {
+            // transform
+            GameObject data = JsonUtility.FromJson<GameObject>(sd.doors[doorName]);
+            GameObject target = GameObject.Find(doorName);
+            target.transform.position = data.transform.position;
+            target.transform.rotation = data.transform.rotation;
+            target.transform.localScale = data.transform.localScale;
+
+            //lock
+            //target.locked = data.locked
+        }
+
+        // load keyItems
+        foreach (var keyItemName in sd.keyItems.Keys)
+        {
+            // transform
+            GameObject data = JsonUtility.FromJson<GameObject>(sd.doors[keyItemName]);
+            GameObject target = GameObject.Find(keyItemName);
+            target.transform.position = data.transform.position;
+            target.transform.rotation = data.transform.rotation;
+            target.transform.localScale = data.transform.localScale;
+
+            // anything else that needs to be loaded
+        }
+
+        // load player
+        foreach (var player in sd.player.Keys)
+        {
+            // transform
+            GameObject data = JsonUtility.FromJson<GameObject>(sd.doors[player]);
+            GameObject target = GameObject.Find(player);
+            target.transform.position = data.transform.position;
+            target.transform.rotation = data.transform.rotation;
+            target.transform.localScale = data.transform.localScale;
+
+            // anything else that needs to be loaded
+        }
+
+        // load moveableObjects
+        foreach (var moveableObjectName in sd.moveableObjects.Keys)
+        {
+            // transform
+            GameObject data = JsonUtility.FromJson<GameObject>(sd.doors[moveableObjectName]);
+            GameObject target = GameObject.Find(moveableObjectName);
+            target.transform.position = data.transform.position;
+            target.transform.rotation = data.transform.rotation;
+            target.transform.localScale = data.transform.localScale;
+
+            // anything else that needs to be loaded
+        }
     }
 }
 
-// make a Saveable interface
-public interface ISaveable
-{
-    // has an Awake or Start function // don't know which would be appropriate
-    void Awake();
-    
-    // adds a reference to its name and type of save script in the SaveManager script
-    // has a Save function
-    string Save();// returns a serialized string
-
-    // has a Load function
-    void Load(string data);// takes an serialized string
-
-}
-
-// each different thing has a script that implements Saveable
-
-// objectData class
+// SaveFileData class
 [Serializable]// serializable
-class ObjectSaveData
+class SaveFileData
 {
-    // contains a list of objects and their serialized data (as strings)
-    public List<String> trackedObjects;
-    public List<String> objectSaveData;
+    public Dictionary<String, String> doors;
+    public Dictionary<String, String> keyItems;
+    public Dictionary<String, String> player;
+    public Dictionary<String, String> moveableObjects;
+
+    public SaveFileData()
+    {
+        doors = new Dictionary<string, string>();
+        keyItems = new Dictionary<string, string>();
+        player = new Dictionary<string, string>();
+        moveableObjects = new Dictionary<string, string>();
+    }
 }
