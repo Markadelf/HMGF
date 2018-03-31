f8470ac4caebd76b9d3fb2796a0cbfe334a1a8cc
MYLAPTOP\Mark <markobeldobel@yahoo.com>
2018-03-31 01:35:42 -0400

    Added the pose like a statue puzzle, missing the actual posing component.

---
+++ Assets/Scripts/PlatformPosing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPosing : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+
+	public bool puzzleSolved;
+
+	private float scaleComponent;
+
+	// Use this for initialization
+	void Start () 
+	{
+		scaleComponent = 1.0f;
+		puzzleSolved = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//positioning is correct
+		if (transform.position.x > player.transform.position.x - scaleComponent && transform.position.x < player.transform.position.x + scaleComponent
+		    && transform.position.z > player.transform.position.z - scaleComponent && transform.position.z < player.transform.position.z + scaleComponent
+			&& player.transform.position.y > transform.position.y)
+		{
+			//TODO: check if the player is posing
+			if (true)
+			{
+				puzzleSolved = true;
+			}
+		}
+		print (puzzleSolved);
+	}
+}
---
+++ Assets/Scripts/ToggleInvisibleCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleInvisibleCollider : MonoBehaviour {
+
+	[SerializeField] private GameObject pedistool;
+
+	// Use this for initialization
+	void Start() 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		if (pedistool.GetComponent<PlatformPosing>().puzzleSolved)
+		{
+			gameObject.GetComponent<BoxCollider>().enabled = false;
+		}
+	}
+}
