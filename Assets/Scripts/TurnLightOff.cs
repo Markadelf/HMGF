4838a97e1ea869620c29688a01aa9afbb728a852
MYLAPTOP\Mark <markobeldobel@yahoo.com>
2018-04-03 22:57:05 -0400

    Added sound files to the project and implemented lighting. Created a new prefab out of the new mansion.

---
+++ Assets/Scripts/TurnLightOff.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLightOff : MonoBehaviour {
+
+	//add to directional light in scene
+	//turns off light when game starts
+
+	private Light myDirectionalLight;
+
+	// Use this for initialization
+	void Start() 
+	{
+		myDirectionalLight = gameObject.GetComponent<Light>();
+		myDirectionalLight.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		
+	}
+}
