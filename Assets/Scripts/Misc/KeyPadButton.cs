57793d9896f64e97b2d670b3fb98ee50cfc9bf60
Mark <Mxd5700@rit.edu>
2018-04-13 14:39:57 -0400

    Work on keypad and textures

---
+++ Assets/Scripts/Misc/KeyPadButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPadButton : Grabable {
+
+    public int Value;
+    public bool pressed;
+    private float _originalZ;
+
+	// Use this for initialization
+	void Start () {
+        _originalZ = transform.localPosition.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (pressed)
+        {
+            transform.localPosition += new Vector3(0, 0, -.01f); 
+        }
+        else if (transform.localPosition.z < _originalZ)
+        {
+            transform.localPosition += new Vector3(0, 0, .01f);
+        }
+        if(transform.localPosition.z < .1f)
+        {
+            pressed = false;
+        }
+        
+	}
+
+    public override void Grab(GameObject grabber)
+    {
+        pressed = true;
+        if (KeyPadManager.active != null)
+            KeyPadManager.active.Notify(Value);
+        
+    }
+
+    public override void Release()
+    {
+    }
+
+}
---
+++ Assets/Scripts/Misc/KeyPadManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPadManager : Trigger {
+
+    public static KeyPadManager active;
+    private int _index = 0;
+    private int[] _code = { 8, 7 };
+
+	// Use this for initialization
+	void Start () {
+        active = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Notify(int val)
+    {
+        if(_code[_index] == val)
+        {
+            _index++;
+            if(_index >= _code.Length)
+            {
+                Activate(true);
+                _index = 0;
+            }
+        }
+        else
+        {
+            _index = 0;
+        }
+    }
+}
