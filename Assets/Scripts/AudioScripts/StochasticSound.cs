f9977a763bf8642f2351c4ec3f9d88cbe11da84d
MYLAPTOP\Mark <markobeldobel@yahoo.com>
2018-04-19 19:03:52 -0400

    Added secrets to the study and implemented a whole lot of sound stuff.

---
+++ Assets/Scripts/AudioScripts/StochasticSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StochasticSound : MonoBehaviour {
+
+	private float timer;
+	private float chance;
+
+	public float frequency;
+
+	// Use this for initialization
+	void Start() 
+	{
+		timer = 0.0f;
+		chance = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		float rand = Random.value;
+
+		if (rand < chance)
+		{
+			AudioSource mySound = gameObject.GetComponent<AudioSource>();
+			mySound.Play();
+			chance = 0.0f;
+		}
+
+
+		timer += Time.deltaTime;
+		chance += Time.deltaTime * 0.00001f * frequency;
+	}
+}
--- Assets/Scripts/TriggerEvents/OpenDoor.cs
+++ Assets/Scripts/TriggerEvents/OpenDoor.cs
@@ -11,6 +11,7 @@ public class OpenDoor : Grabable {
 	[SerializeField] private bool isLocked;
 	//determines if the door is currently closed
 	private bool doorClosed;
+	private bool doorClosedSoundPlayed;
 	public GameObject player;
 	//determines if the door opens clockwise from above
 	public bool clockwise;
@@ -24,6 +25,7 @@ public class OpenDoor : Grabable {
 		timeTillReclick = 60.0f;
 		timer = 0.0f;
 		doorClosed = true;
+		doorClosedSoundPlayed = true;
 		startYRot = transform.eulerAngles.y;
 		currYRot = startYRot;
 	}
@@ -35,6 +37,20 @@ public class OpenDoor : Grabable {
 		{
 			doorClosed = !doorClosed;
 			timer = 0.0f;
+
+			//if opening the door, play the open door sound, and make available the close door sound
+			if (!doorClosed)
+			{
+				AudioSource doorOpenSound = GameObject.FindGameObjectWithTag("DoorOpen").GetComponent<AudioSource>();
+				doorOpenSound.Play();
+				doorClosedSoundPlayed = false;
+			}
+		}
+		//if a player tries to open the door when it is locked, play the sound effect
+		else if (Vector3.Distance(player.transform.position, transform.position) < 4.0f && Input.GetAxis("Interact") > 0 && timer > timeTillReclick && isLocked)
+		{
+			AudioSource doorLockedSound = GameObject.FindGameObjectWithTag("DoorLocked").GetComponent<AudioSource>();
+			doorLockedSound.Play();
 		}
 
 		if (!clockwise)
@@ -42,10 +58,17 @@ public class OpenDoor : Grabable {
 			if (doorClosed && currYRot < startYRot)
 			{
 				currYRot++;
-			} 
+			}
 			else if (!doorClosed && currYRot > startYRot - 90.0f)
 			{
 				currYRot--;
+			}
+			//when the door shuts, play the close sound effect; the door must be opened to play it again
+			else if (doorClosed && currYRot >= startYRot && !doorClosedSoundPlayed)
+			{
+				AudioSource doorCloseSound = GameObject.FindGameObjectWithTag("DoorClose").GetComponent<AudioSource>();
+				doorCloseSound.Play();
+				doorClosedSoundPlayed = true;
 			}
 		} 
 		else
@@ -58,6 +81,13 @@ public class OpenDoor : Grabable {
 			{
 				currYRot++;
 			}
+			//when the door shuts, play the close sound effect; the door must be opened to play it again
+			else if (doorClosed && currYRot <= startYRot && !doorClosedSoundPlayed)
+			{
+				AudioSource doorCloseSound = GameObject.FindGameObjectWithTag("DoorClose").GetComponent<AudioSource>();
+				doorCloseSound.Play();
+				doorClosedSoundPlayed = true;
+			} 
 		}
 
 		transform.rotation = Quaternion.Euler(transform.rotation.x, currYRot, transform.rotation.z);
