0769280937650bcede04a60f5525aa108c96fc2b
Mark DelFavero <Mxd5700@rit.edu>
2018-04-19 21:52:02 -0400

    Made stuff prettier.

---
+++ Assets/Scripts/Misc/RandomColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var rend = GetComponent<Renderer>();
+        rend.material.color = new Color(Random.value, Random.value, Random.value);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
